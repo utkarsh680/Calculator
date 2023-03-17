67f442e244d68d3ae6e9269c6b1b805f010e512a
utkarsh <singhrishu680@gmail.com>
2023-03-18 00:59:18 +0530

    add dark and light mode

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 var buttons = document.getElementsByClassName('button');
 var display = document.getElementById('screen-input');
 var result = document.getElementById('screen-result');
+var screen = document.getElementById('screen');
+
+var mode = document.getElementById('mode');
+var body = document.getElementsByTagName('body')[0];
+
+mode.addEventListener('click', function(){
+    body.classList.toggle('dark');  
+    display.classList.toggle('dark');
+    result.classList.toggle('dark');
+    screen.classList.toggle('dark');
+    mode.classList.toggle('dark');
+    if(mode.classList.contains('dark')){
+        theme.src = "https://cdn-icons-png.flaticon.com/512/581/581601.png"
+    }else{
+        theme.src = "https://cdn-icons-png.flaticon.com/512/439/439842.png" 
+    }
+    
+
+})
+var theme = document.getElementById('theme');
+
+
+
+// if(body.classList.contains('dark')){
+//     body.style.backgroundColor = 'black';
+//     body.style.color = 'white';
+// }else{
+//     body.style.backgroundColor = 'white';
+//     body.style.color = 'black';
+// }
+
 
 
 function button(num){
@@ -16,14 +47,13 @@ function Clear(){
 function Calculate(){
     try{
       result.innerHTML = eval(display.value);
-     
       }
       catch(err){
           display.value = "Error";
       }
 }
 function Del(){
-    
     display.value = display.value.substr(0,display.value.length-1);
     
 }
+
