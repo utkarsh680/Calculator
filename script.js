cd5a537fded3d6a2f1b0c5bdf7c8fbd9ef83a51d
utkarsh <singhrishu680@gmail.com>
2023-03-17 18:05:23 +0530

    adding input output field

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 var buttons = document.getElementsByClassName('button');
 var display = document.getElementById('screen-input');
+var result = document.getElementById('screen-result');
+
 
 function button(num){
     console.log(num);
     display.value+= num;
+    
 }
 function Clear(){
-    console.log("hhh")
     display.value = "";
+    result.innerHTML = "";
+    console.log(result.value)
 }
 function Calculate(){
     try{
-        display.value = eval(display.value);
-    }
-    catch(err){
-        display.value = "Error";
-    }
+      result.innerHTML = eval(display.value);
+     
+      }
+      catch(err){
+          display.value = "Error";
+      }
 }
 function Del(){
-    display.value = value.substr(0,value.length-1);
+    
+    display.value = display.value.substr(0,display.value.length-1);
+    
 }
